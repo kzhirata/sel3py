61e317c8cab433186ff38ec6493327e7e449dfcb
kzhirata <kzhirata77@gmail.com>
2020-03-13 11:02:03 +0900

    first commit

---
+++ testc2.py
@@ -0,0 +1,17 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+capabilities = DesiredCapabilities.CHROME.copy()
+capabilities['enableVNC'] = True
+capabilities['enableVideo'] = True
+capabilities['videoName'] = 'my-video-c2.mp4'
+driver = webdriver.Remote(
+    command_executor='http://localhost:4444/wd/hub',
+    desired_capabilities=capabilities)
+driver.get('https://google.com')
+element = driver.find_element_by_name('q')
+element.send_keys('生産性向上ブログ')
+element.submit()
+driver.quit()
